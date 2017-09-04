d27e445e6544d949cc0c34b1ffc680b0d416e8ab
almgwary <almgwaryfci@gmail.com>
2017-09-04 18:20:52 +0200

    run and use facebook sdk

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,45 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  isLoggedIn:boolean = false ;
+  data = {}  ;
+  constructor(public navCtrl: NavController, private fb: Facebook) {
 
   }
 
+
+  facebookLogin(){
+
+    this.fb.login(['public_profile', 'user_friends', 'email'])
+      .then((res: FacebookLoginResponse) =>{
+        console.log('Logged into Facebook!', res);
+        this.isLoggedIn = true ;
+        this.data = res ;
+      })
+      .catch(e => {
+        console.log('Error logging into Facebook', e)
+      });
+
+
+    // this.fb.logEvent(this.fb.EVENTS.EVENT_NAME_ADDED_TO_CART);
+  }
+
+  facebookLogout(){
+    this.fb.logout()
+      .then((res: any) =>{
+        console.log('Logged out Facebook!', res);
+        this.isLoggedIn = false ;
+        this.data = {} ;
+      })
+      .catch(e => {
+        console.log('Error logging out Facebook', e)
+      });
+  }
 }
